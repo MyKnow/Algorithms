a2768ae96cd5c3a22e3f109deae457359a4e6681
MyKnow <56116733+MyKnow@users.noreply.github.com>
2024-10-14 17:48:18 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -2,25 +2,12 @@
 dice1, dice2, dice3 = map(int, input().split())
 
 # 같은 눈이 3개 나오는 경우
-if (dice1 == dice2 and dice2 == dice3) :
-    print(10000+dice1*1000)
+if dice1 == dice2 == dice3:
+    print(10000 + dice1 * 1000)
 # 모두 다른 눈이 나오는 경우
-elif (dice1 != dice2 and dice2 != dice3 and dice1 != dice3) :
-    # 가장 큰 값 찾기
-    lagestNumber = dice1
-    if (dice2 > dice1 and dice2 > dice3) :
-        lagestNumber = dice2
-    elif (dice3 > dice2 and dice3 > dice1) :
-        lagestNumber = dice3
-        
-    print(lagestNumber*100)
+elif dice1 != dice2 != dice3 != dice1:
+    print(max(dice1, dice2, dice3) * 100)
 # 같은 눈이 2개만 나오는 경우
-else :
-    pairDiceNumber = 0
-    if (dice1 == dice2 or dice1 == dice3) :
-        pairDiceNumber = dice1
-    else :
-        pairDiceNumber = dice2
-        
-    print(1000+pairDiceNumber*100)
-    
+else:
+    pairDiceNumber = dice1 if dice1 == dice2 or dice1 == dice3 else dice2
+    print(1000 + pairDiceNumber * 100)

034a4d6d79d2be301f59f6f1d03c7bac0c19cea0
MyKnow <56116733+MyKnow@users.noreply.github.com>
2024-12-31 20:27:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 396 ms, Memory: 133696 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.swift
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.swift
@@ -27,6 +27,26 @@ func getResultWithoutMethod(targetArray: Array<Int>) -> Array<Int> {
     return [minOfArray, maxOfArray]
 }
 
+/// [func] getResultWithMethod
+/// 
+/// # Notes
+/// - min, max 함수를 사용하여 최소값, 최대값을 산출해내는 함수
+/// 
+/// # Parameters
+/// - Array<Int> targetArray : 값을 산출하고자 하는 정수 배열
+/// 
+/// # Returns
+/// - Array<Int> result : [minOfArray, maxOfArray]
+/// 
+func getResultWithMethod(targetArray: Array<Int>) -> Array<Int> {
+    // [Int] minOfArray : 배열의 최소값을 저장하는 변수
+    var minOfArray: Int = (targetArray.min() ?? 0)
+    // [Int] maxOfArray : 배열의 최대값을 저장하는 변수
+    var maxOfArray: Int = (targetArray.max() ?? 0)
+  
+    return [minOfArray, maxOfArray]
+}
+
 
 // [Int] N : 정수의 개수 (1 <= N <= 1,000,000)
 let N: Int = Int(readLine() ?? "1") ?? 1
@@ -34,6 +54,6 @@ let N: Int = Int(readLine() ?? "1") ?? 1
 // [Array<Int>] arr : Int를 저장하는 배열 (-1,000,000 <= int <= 1,000,000)
 let arr: Array<Int> = (readLine() ?? "0").split(separator: " ").map{Int($0) ?? 0}
 
-let resultArr = getResultWithoutMethod(targetArray: arr)
+let resultArr = getResultWithMethod(targetArray: arr)
 
 print(resultArr[0], resultArr[1])

4b31cdb467ff452570078fedbb11251fdf820c61
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-01-25 18:58:17 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 8 ms, Memory: 69092 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.swift
@@ -0,0 +1,5 @@
+// [String] inputOfString (immutable) : 알파벳 소문자, 대문자, 공백, 숫자로만 이루어져 있는 문자열 (길이: 100줄 미만)
+while let inputOfString: String = readLine() {
+    // 값을 받는 한 계속 출력
+    print(inputOfString)
+}

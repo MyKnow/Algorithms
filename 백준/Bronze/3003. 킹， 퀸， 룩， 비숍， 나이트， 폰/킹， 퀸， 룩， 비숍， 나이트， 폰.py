37a89deb17cb8c61df0557b1dc446060e4377b96
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-01-28 17:28:21 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,21 +1,13 @@
-# [list(int)] listOfPiece : 동혁이가 찾은 흰색 체스 기물 개수 리스트 (0 <= int <= 10)
-listOfPiece = list(map(int, input().split()))
+# [list(int)] numberOfNowPiece : 동혁이가 찾은 흰색 체스 기물 개수 리스트 (0 <= int <= 10)
+numberOfNowPiece = list(map(int, input().split()))
 
+# [list(int)] numberOfRequiredPieces :
 # 체스 기물 중에서
 # 킹과 퀸은 1개
 # 룩, 비숍, 나이트는 2개
 # 폰은 8개
-# 이므로, 순서에 따라 조건을 다르게 검사한다.
-for index in range(len(listOfPiece)) :
-    # [int] numberOfRequired: 필요한 기물 수 (초기 값은 킹, 퀸, 폰을 제외한 각 기물의 필요 기물 수)
-    numberOfRequired = 2
-    
-    # 킹 또는 퀸일 떄
-    if (index == 0 or index == 1) :
-        numberOfRequired = 1
-    # 폰일 때
-    elif (index == len(listOfPiece)-1) :
-        numberOfRequired = 8
+# 이므로, 제약 조건을 설정하는 리스트를 생성한다.
+numberOfRequiredPieces = [1, 1, 2, 2, 2, 8]
 
-    # 필요 기물 수에서 현재 index의 기물 수를 뺀 수를 출력한다.
-    print(numberOfRequired - listOfPiece[index], end=" ")
+# 필요한 수에서 현재 수를 뺀 값을 출력
+print(" ".join(str(r-n) for r, n in zip(numberOfRequiredPieces, numberOfNowPiece)))

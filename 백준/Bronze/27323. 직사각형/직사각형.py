5da27532a51d0ab62e9334a2611cae6d00b4c4e2
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-01 18:26:17 +0900

    [Bronze V] Title: 직사각형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27323. 직사각형/직사각형.py
+++ 백준/Bronze/27323. 직사각형/직사각형.py
@@ -1,8 +1,6 @@
-# stdin을 위한 sys import
-import sys
-
 # [int] A, B: 직사각형의 세로, 가로 길이 (범위: 1 <= A, B <= 100의 자연수)
-A, B = map(int, sys.stdin.read().strip().split())
+A = int(input())
+B = int(input())
 
 # 넓이 출력
 print(A*B)

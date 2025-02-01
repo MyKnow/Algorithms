37310b82522b554c0033442adc8f35db64f7ebb3
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-01 18:13:06 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -6,11 +6,16 @@ if (N != 1):
     # [list(int)] resultOfPrimeFactorization: 소인수분해를 진행한 결과를 저장하는 리스트
     resultOfPrimeFactorization = []
 
-    for number in range(2, N+1):
+    # 2부터 N의 제곱근까지만 나눠서 소인수를 찾으면 훨씬 빠르다.
+    for number in range(2, int(N ** 0.5) + 1):
         # number로 나눌 수 없을 때까지 반복한다.
         while(N % number == 0):
             resultOfPrimeFactorization.append(number)
             N //= number
-
+        
+    # 소인수 분해를 진행하고 남은 N도 리스트에 추가한다.
+    if (N > 1):
+        resultOfPrimeFactorization.append(N)
+            
     # resultOfPrimeFactorization의 요소들을 출력
     print("\n".join(map(str, resultOfPrimeFactorization)))

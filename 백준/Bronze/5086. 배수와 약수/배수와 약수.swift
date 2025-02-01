769f8d2b96bb72e27cd3bc14c55fca4232c95dba
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-01 14:38:09 +0900

    [Bronze III] Title: 배수와 약수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.swift
@@ -0,0 +1,25 @@
+// 특정 input을 만나기 전까지 무한반복
+while(true) {
+    // [Int] n1, n2 (immutable): 약수와 배수 관계를 계산할 두 자연수 (범위: 0 < n < 10,000)
+    let inputArray: Array<Int> = (readLine() ?? "1 1").split(separator: " ").map { Int($0) ?? 1 }
+    let n1: Int = inputArray[0]
+    let n2: Int = inputArray[1]
+
+    // n1, n2 둘 다 0인 경우 종료한다.
+    if (n1 == 0 && n2 == 0) {
+        break
+    }
+    
+    // 첫 번째 숫자가 두 번째 숫자의 약수인 경우.
+    if (n2 % n1 == 0) {
+        print("factor")
+    }
+    // 첫 번째 숫자가 두 번째 숫자의 배수인 경우.
+    else if (n1 % n2 == 0) {
+        print("multiple")
+    }
+    // 둘 다 아니라면
+    else {
+        print("neither")
+    }
+}

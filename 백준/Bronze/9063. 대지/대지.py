082521b9058e1041652f03193a0eab401d24a10d
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-02 14:20:14 +0900

    [Bronze III] Title: 대지, Time: 1356 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,22 +1,20 @@
 #[int] N: 좌표의 개수 (범위: 1 <= N <= 100,000)
 N = int(input())
-
-# [list[tuple]] listOfPoint: 각 좌표 튜플을 저장하는 리스트 (길이: N)
-listOfPoint = []
+    
+# [int] min_x, min_y, max_x, max_y : 좌하단과 우상단 좌표를 저장하는 튜플
+# 각 변수의 초기값은 의도에 맞게 입력 범위의 최대값, 최소값으로 초기화한다.
+min_x, min_y = 10000, 10000
+max_x, max_y = -10000, -10000
 
 for _ in range(N):
-    # 입력 받은 좌표를 리스트에 추가한다. (범위(정수): -10,000 <= point <= 10,000)
-    listOfPoint.append(tuple(map(int, input().split())))
+    # [int] x, y: 입력 받은 좌표 (범위(정수): -10,000 <= x, y <= 10,000)
+    x, y = map(int, input().split())
     
-# [tuple(int, int)] rightTopPoint, leftBottomPoint : 좌하단과 우상단 좌표를 저장하는 튜플
-# 각 튜플의 초기값은 의도에 맞게 입력 범위의 최대값, 최소값으로 초기화한다.
-rightTopPoint = (-10000, -10000)
-leftBottomPoint = (10000, 10000)
-
-for x, y in listOfPoint:
     # 각 좌표를 조건에 맞게 변경해나간다.
-    rightTopPoint = (max(rightTopPoint[0], x), max(rightTopPoint[1], y))
-    leftBottomPoint = (min(leftBottomPoint[0], x), min(leftBottomPoint[1], y))
-
+    min_x = min(min_x, x)
+    min_y = min(min_y, y)
+    max_x = max(max_x, x)
+    max_y = max(max_y, y)
+    
 # 좌하단, 우상단 좌표를 통해 넓이 계산 및 출력
-print((rightTopPoint[1]-leftBottomPoint[1]) * (rightTopPoint[0]-leftBottomPoint[0]))
+print((max_x - min_x) * (max_y - min_y))

fbb26e0f7d4aef7ca76d8ff07e7b47a0c162c7d2
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-04 20:23:54 +0900

    [Bronze II] Title: 수 정렬하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -0,0 +1,8 @@
+import sys
+
+_ = int(input())
+
+# [list(int)] sortedList: 입력된 숫자를 저장하고 정렬한 리스트 (길이: 1 <= N <= 1,000)
+sortedList = sorted(list(map(int, (sys.stdin.read().split()))))
+
+print("\n".join(map(str, sortedList)))

bca0b61345148308495bad0e6f814dbc29ce940a
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-04 20:10:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,24 @@
+# [int] N: 배달해야 하는 설탕의 무게 (범위(자연수): 3 <= N <= 5,000)
+N = int(input())
+
+# [int] count: 봉지의 수
+count = 0
+
+# [int] remainingWeight: 남은 설탕의 무게
+remainingWeight = N
+
+# 남은 설탕이 없어질 때까지 반복
+while (remainingWeight >= 0):
+    # 현재 5kg 봉지로 모두 담을 수 있다면 바로 종료
+    if (remainingWeight % 5 == 0) :
+        # 현재 봉지 수에 5kg 봉지를 모두 더한다.
+        count += remainingWeight // 5
+        print(count)
+        break
+    # 5kg 봉지로 다 담을 수 없다면, 3kg 봉지로 담는다.
+    count += 1
+    remainingWeight -= 3
+
+# 만약 남은 설탕이 음수라면, 정확하게 만들 수 없는 것이므로 -1 출력
+if (remainingWeight < 0):
+    print(-1)

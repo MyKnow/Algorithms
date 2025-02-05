a41dfa15a56390f14ccacd0a62305b1ccb35bfe2
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-05 11:53:02 +0900

    [Bronze II] Title: 커트라인, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.swift
@@ -0,0 +1,9 @@
+// [Int] N, k (immutable): 응시자의 수와 상을 받는 사람의 수 (범위(자연수): 1 <= k <= N <= 1,000)
+let arrayOfInput: Array<Int> = (readLine() ?? "1 1").split(separator: " ").compactMap { Int($0) }
+let (N, k): (Int, Int) = (arrayOfInput[0], arrayOfInput[1])
+
+// [Array<Int>] arrayOfPoints (immutable): N개의 점수들을 저장하는 배열 (길이: N)
+let arrayOfPoints: Array<Int> = (readLine() ?? "1 1").split(separator: " ").compactMap { Int($0) }
+
+// 배열을 내림차순 정렬하고, k-1번째 점수를 출력한다.
+print( (arrayOfPoints.sorted().reversed())[k-1] )

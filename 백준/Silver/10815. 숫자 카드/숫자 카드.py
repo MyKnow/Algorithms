83c367013e4bdb4a4b70c3d95541c19ae6059b36
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-06 13:45:00 +0900

    [Silver V] Title: 숫자 카드, Time: 712 ms, Memory: 110896 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,16 @@
+# [int] N: 상근이가 가지고 있는 숫자 카드의 개수 (범위(자연수): 1 ≤ N ≤ 500,000)
+N = int(input())
+
+# [dict(number: number)] dictOfCards: 상근이가 가지고 있는 카드를 저장하는 사전 (크기: N)
+dictOfCards = dict()
+for number in map(int, input().split()) :
+    dictOfCards[number] = 1
+
+# [int] M: 입력될 정수의 개수 (범위(자연수): 1 ≤ M ≤ 500,000)
+M = int(input())
+
+for number in map(int, input().split()):
+    if (number in dictOfCards.keys()):
+        print(1, end = " ")
+    else :
+        print(0, end = " ")

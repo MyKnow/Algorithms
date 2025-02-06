fe34eadf16431cdb96278911b8906731c562a2ac
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-06 14:32:45 +0900

    [Silver IV] Title: 문자열 집합, Time: 96 ms, Memory: 74956 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.swift
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.swift
@@ -9,18 +9,10 @@ for _ in 0..<N {
     setOfWords.insert( (readLine() ?? "a") )
 }
     
-// [Array<String>] arrayOfWords (mutable): M개의 문자열을 저장할 배열 (크기: M)
-var arrayOfWords: Array<String> = [String]()
-for _ in 0..<M {
-    arrayOfWords.append( (readLine() ?? "a") )
-}
-
 // [Int] count (mutable): 집합 S에 포함된 단어의 개수를 저장하는 변수
 var count: Int = 0
-
-// setOfWords에 arrayOfWords의 단어가 포함되어 있으면 count를 1 증가시킨다.
-for word in arrayOfWords {
-    if (setOfWords.contains(word)) {
+for _ in 0..<M {
+    if (setOfWords.contains( (readLine() ?? "a") )) {
         count+=1
     }
 }

63b1a5fc83780b78cb22421fa875247575fb2ad9
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-06 18:54:43 +0900

    [Silver IV] Title: 듣보잡, Time: 2568 ms, Memory: 44192 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,28 @@
+import sys
+
+# input = sys.stdin.readline
+
+# [int] N, M
+# N: 듣도 못한 사람의 수 
+# M: 보도 못한 사람의 수
+# (범위(자연수): <= 20)
+N, M = map(int, input().split())
+
+# [set(int)] setOfN: 듣도 못한 사람을 저장하는 집합 (길이: N)
+setOfN = set()
+
+# [set(int)] setOfM: 보도 못한 사람을 저장하는 집합 (길이: M)
+setOfM = set()
+
+for _ in range(N):
+    setOfN.add(input())
+    
+for _ in range(M):
+    setOfM.add(input())
+    
+# [set(int)] setOfIntersection: 듣도 보도 못한 사람을 저장하는 집합 (길이: <= N, M)
+setOfIntersection = setOfN.intersection(setOfM)
+
+# 출력
+print(len(setOfIntersection))
+print("\n".join(sorted(list(setOfIntersection))))

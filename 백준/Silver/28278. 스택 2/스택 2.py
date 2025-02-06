58bf5d64f54e960264448cff2187d67b8a1682fa
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-06 21:07:25 +0900

    [Silver IV] Title: 스택 2, Time: 1016 ms, Memory: 88788 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -8,6 +8,9 @@ N = int(input())
 # [list(int)] stack: 정수를 저장할 스택
 stack = list()
 
+# [list(str)] listOfResult: 답을 저장하는 리스트
+listOfResult = list()
+
 for _ in range(N):
     order = input().strip()
     
@@ -15,26 +18,28 @@ for _ in range(N):
     # 1. X를 스택에 넣기.
     if (len(order) > 1) :
         _, x = map(int, order.split())
-        stack.append(x)
+        stack.append(str(x))
     # 2. 스택에 정수가 존재할 경우 빼고 출력한다.
     # 없다면 -1을 대신 출력한다.
     elif (int(order) == 2) :
         if (len(stack) > 0) :
-            print(stack.pop())
+            listOfResult.append(stack.pop())
         else :
-            print(-1)
+            listOfResult.append("-1")
     # 3. 스택에 들어있는 정수의 개수를 출력한다.
     elif (int(order) == 3):
-        print(len(stack))
+        listOfResult.append(str(len(stack)))
     # 4. 스택이 비어있으면 1, 아니면 0을 출력한다.
     elif (int(order) == 4):
         if (len(stack) == 0):
-            print(1)
+            listOfResult.append("1")
         else :
-            print(0)
+            listOfResult.append("0")
     # 5. 스택에 정수가 있다면 맨 위의 정수를 출력한다. 없다면 -1을 출력한다.
     else:
         if (len(stack) > 0):
-            print(stack[len(stack)-1])
+            listOfResult.append(str(stack[-1]))
         else:
-            print(-1)
+            listOfResult.append("-1")
+            
+print("\n".join(listOfResult))

cc718373fc6217556c46049f7760bfa82e86b4fc
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-06 20:07:19 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 160 ms, Memory: 81612 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.swift
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.swift
@@ -1,8 +1,8 @@
 // [Array<String>] S (immutable): 서로 다른 부분 문자열의 개수를 구하고자 하는 원본 문자열로 만든 배열
 let S: Array<String> = (readLine() ?? "ababc").map { String($0) }
 
-// [Set<String>] setOfDifference (mutable): 서로 다른 부분 문자열을 저장하는 집합
-var setOfDifference: Set<String> = Set<String>()
+// [Set<Int>] setOfDifference (mutable): 서로 다른 부분 문자열을 해쉬화한 값을 저장하는 집합
+var setOfDifference: Set<Int> = Set<Int>()
 
 for i in 0..<S.count {
     // [String] str (mutable): 문자를 조합할 때 사용할 변수
@@ -10,7 +10,7 @@ for i in 0..<S.count {
     
     for j in i..<S.count {
         str += String(S[j])
-        setOfDifference.insert(str)
+        setOfDifference.insert(str.hashValue)
     }
 }
         

d8cf96f44bac2c3f293afb4704e0c331f43eff02
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-17 17:19:49 +0900

    [Silver III] Title: 소수 구하기, Time: 32 ms, Memory: 83604 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.swift
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.swift
@@ -1,36 +1,34 @@
 import Foundation
 
-// [func(Int)->Bool] isPrimeNumber: 주어진 정수가 소수인지 확인하는 함수
-func isPrimeNumber(_ integer: Int) -> Bool {
-    // 0, 1은 소수가 아니다.
-    if (integer <= 1) {
-        return false
-    }
-    // 2, 3은 소수이다.
-    else if (integer <= 3) {
-        return true
-    }
+// [func(Int)->Array<Int>] getPrimeNumberArray: 1,000,000 이하의 소수로 구성된 배열
+func getPrimeNumberArray(_ integer: Int) -> Array<Int> {
+    // [Array<Bool>] boolArray (mutable): 0부터 1,000,000까지의 index를 가지는 배열 (길이: 1,000,001)
+    var boolArray: Array<Bool> = [Bool](repeating: true, count: integer+1)
     
     // [Int] sqrtInt (immutable): 제곱근이 취해진 integer
-    let sqrtInt: Int = Int(Double(integer).squareRoot())
+    let sqrtInt: Int = Int(Double(integer+1).squareRoot())
     
-    // 2부터 자기자신 이전까지 나누어 떨어지지 않으면 소수이다.
-    for number in 2...sqrtInt {
-        // 나누어 떨어지는 경우가 있으므로 즉시 False 반환
-        if (integer % number == 0) {
-            return false
+    // 2부터 자기자신 이전까지 소수가 존재하는 지 검사
+    for n in 2...sqrtInt+1 {
+        // boolArray의 n번째 요소가 소수라면, n의 배수들은 소수가 아니다.
+        if (boolArray[n]) {
+            var i: Int = 2
+            
+            while (n * i <= integer) {
+                boolArray[n*i] = false
+                i+=1
+            }
         }
     }
     
-    return true
+    // index가 2 이상이고, value가 true인 인덱스를 묶어서 반환
+    return boolArray.enumerated().filter { $0.offset >= 2 && $0.element }.map { $0.offset }
 }
 
 // [Int] M, N (immutable): 소수를 찾을 정수의 범위 (범위(자연수): 1 ≤ M ≤ N ≤ 1,000,000)
 let arrayOfInput: Array<Int> = (readLine() ?? "1 1").split(separator: " ").compactMap { Int($0) }
 let (M, N): (Int, Int) = (arrayOfInput[0], arrayOfInput[1])
 
-for n in M...N {
-    if (isPrimeNumber(n)) {
-        print(n)
-    }
-}
+// getPrimeNumberArray에서 얻은 배열에서 M과 N 사이의 값만 출력
+let result: Array<Int> = getPrimeNumberArray(N).filter { (M...N).contains($0) }
+print(result.map { String($0) }.joined(separator: "\n"))

3b0cd76df8f06512aa9e8d13961c5ace602759f2
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-17 19:33:22 +0900

    [Silver II] Title: 베르트랑 공준, Time: 72 ms, Memory: 35440 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -17,11 +17,37 @@ def getPrimeNumberList(integer):
             # n의 배수들을 모두 False로 설정 (n * n부터 시작)
             for i in range(n * n, integer + 1, n):
                 primeList[i] = False
-    # primeList에서 index가 integer//2보다 크며, 값이 True인 index만 모아서 정수 리스트로 반환한다.
+    # primeList에서 index가 2보다 크거나 같으며, 값이 True인 index만 모아서 정수 리스트로 반환한다.
     # [list(int)] result: 위 조건에 맞는 값을 저장하는 리스트
-    result = [index for index in range(integer//2+1, integer + 1) if primeList[index]]
+    result = [index for index in range(2, integer + 1) if primeList[index]]
     
     return result
+
+# [def(list(int), int) -> int] binarySearch: 리스트에서 목표 값의 위치를 반환하는 함수
+def binarySearch(data, target):
+    # 1. data는 이미 정렬되어 있으므로, 정렬 과정 생략.
+    
+    # 2. left, right 선언
+    left = 0
+    right = len(data) - 1
+    
+    # 3. left가 right보다 커지면 종료
+    while (left <= right):
+        # 4. mid 선언
+        mid = (left+right)//2
+        
+        # 5. mid가 target보다 크다면 right를 mid-1로 업데이트
+        if (data[mid] > target):
+            right = mid - 1
+        # 6. mid가 target보다 작거나 같다면 left를 mid+1로 업데이트
+        else:
+            left = mid + 1
+            
+    # 8. left 출력
+    return left
+
+# [list(int)] listOfPrimeNumbers: 123,456 * 2보다 작은 소수들을 저장한 리스트
+listOfPrimeNumbers = getPrimeNumberList(123456 * 2)
 
 # [int] n: 소수의 개수를 구하기 위한 범위 (범위(자연수): 1 ≤ n ≤ 123,456)
 while True:
@@ -30,4 +56,4 @@ while True:
     if (n == 0):
         break
         
-    print(len(getPrimeNumberList(n*2)))
+    print(binarySearch(listOfPrimeNumbers, n*2)-binarySearch(listOfPrimeNumbers, n))

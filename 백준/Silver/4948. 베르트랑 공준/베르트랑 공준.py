3f5372d13ba70d1c0f3471d2107e6707abfc05ee
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-02-17 18:03:40 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1212 ms, Memory: 36756 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -14,20 +14,13 @@ def getPrimeNumberList(integer):
     for n in range(2, sqrtInt+1):
         # n이 소수면 n의 배수는 소수가 아님.
         if (primeList[n]):
-            i = 2
-            
-            # integer까지 n*i 번째 요소를 모두 False로 바꿈
-            while (n * i <= integer):
-                primeList[n*i] = False
-                i += 1
-    # primeList에서 index가 2 이상이며, 값이 True인 index만 모아서 정수 리스트로 반환한다.
+            # n의 배수들을 모두 False로 설정 (n * n부터 시작)
+            for i in range(n * n, integer + 1, n):
+                primeList[i] = False
+    # primeList에서 index가 integer//2보다 크며, 값이 True인 index만 모아서 정수 리스트로 반환한다.
     # [list(int)] result: 위 조건에 맞는 값을 저장하는 리스트
-    result = []
+    result = [index for index in range(integer//2+1, integer + 1) if primeList[index]]
     
-    for index, value in list(enumerate(primeList)):
-        if (index > (integer//2) and value):
-            result.append(index)
-            
     return result
 
 # [int] n: 소수의 개수를 구하기 위한 범위 (범위(자연수): 1 ≤ n ≤ 123,456)

f06a01bc396c65ae19a781736126f2a11b7986a6
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-03-02 18:28:18 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 64 ms, Memory: 76036 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.swift
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.swift
@@ -13,15 +13,11 @@ for _ in 0..<N {
     if let log: String = readLine() {  
         // 새로운 사람이 입장했다면 person을 초기화함.
         if (log == "ENTER") {
-            person = Set<String>()
+            person.removeAll()
         }
-        // 그 외의 경우 채팅을 입력한 유저의 닉네임임.
-        else {
-            // 집합에 없는 닉네임인 경우, 집합에 추가하고 count를 증가시킴.
-            if (person.contains(log) == false) {
-                person.insert(log)
-                count+=1
-            }
+        // 집합에 추가되는 닉네임인 경우 count를 증가시킴.
+        else if (person.insert(log).inserted) {
+            count+=1
         }
     }
 }

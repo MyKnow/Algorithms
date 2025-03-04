a1d974d3f214851388fa94deb6f58518a3db7de1
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-03-04 17:53:36 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 236 ms, Memory: 52680 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -4,19 +4,38 @@ from collections import Counter
 input = sys.stdin.readline
 
 # [Int] N, M
+# N: 단어의 수, (범위(자연수): 1 ≤ N ≤ 100,000)
+# M: 단어 길이 제한, (범위(자연수): 1 ≤ M ≤ 10)
 N, M = map(int, input().split())
 
-# [Counter] cntOfWords: 단어 빈도 저장
-cntOfWords = Counter()
+# [list(int)] words: 길이가 M 이상인 단어를 저장하는 리스트
+words = list()
 
 for _ in range(N):
     word = input().strip()
     
-    if len(word) >= M:  # M 이상 길이만 저장
-        cntOfWords[word] += 1
+    # M보다 짧은 길이의 단어는 고려하지 않는다.
+    if (len(word) < M):
+        continue
+    
+    # M 이상인 단어는 단어장에 추가한다.
+    words.append(word)
+    
+# 3. 알파벳 사전 순으로 오름차순 정렬한다.
+words.sort()
 
-# 단어 정렬 (빈도, 길이, 알파벳 순)
-sorted_words = sorted(cntOfWords.keys(), key=lambda word: (-cntOfWords[word], -len(word), word))
+# 2. 길이 순으로 내림차순 정렬한다.
+words.sort(key=lambda word: len(word), reverse=True)
 
-# 한 번에 출력
-sys.stdout.write("\n".join(sorted_words) + "\n")
+
+# [Counter] cntOfWords: 단어장 카운터
+cntOfWords = Counter(words)
+
+# [list(str)] result: 결과
+result = list()
+
+# 1. 빈도 순으로 내림차순 정렬하여 출력한다.
+for key, value in cntOfWords.most_common():
+    result.append(key)
+    
+print("\n".join(result))

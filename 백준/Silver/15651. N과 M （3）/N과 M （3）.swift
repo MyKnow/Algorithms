9a7d1ee05521df12922670d7bbbd93344c73881a
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-03-09 21:10:24 +0900

    [Silver III] Title: N과 M (3), Time: 732 ms, Memory: 111008 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.swift
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.swift
@@ -1,18 +1,28 @@
+import Foundation
+
+// [extension Array<Int>] joinedString
+// : String 확장 함수로 배열을 바로 문자열로 변환하는 최적화 추가
+extension Array where Element == Int {
+    var joinedString: String {
+        return map { String($0) }.joined(separator: " ")
+    }
+}
+
 // [func(inout Array<Int>, inout Array<String>, Int, Int)] printSequence
 // : 주어진 조건에 맞는 수열을 results에 저장하는 함수
 // [inout Array<Int>] stack: 현재 DFS로 탐색 중인 수를 저장하는 배열
-// [inout Array<String>] results: 생성된 수열을 저장하는 배열
+// [inout String] result: 생성된 수열을 저장하는 문자열
 // [Int] N: 자연수의 범위 (범위(자연수): 1 ≤ M ≤ N ≤ 8)
 // [Int] M: 수열의 길이 (범위(자연수): 1 ≤ M ≤ N ≤ 8)
 func printSequence(
     _ stack: inout [Int], 
-    _ results: inout [String],
+    _ result: inout String,
     _ N: Int, 
     _ M: Int
 ) {
     // 1. 만약 스택의 길이가 M과 같다면 리스트에 저장한다.
     if (stack.count == M) {
-        results.append(stack.map { String($0) }.joined(separator: " "))
+        result += stack.joinedString + "\n"
         return
     }
     // 2. 1부터 N까지 DFS를 진행한다.
@@ -20,7 +30,7 @@ func printSequence(
         // 3. 현재 index를 스택에 넣는다.
         stack.append(index)
         // 4. 현재 index를 포함하여 재귀적으로 호출한다.
-        printSequence(&stack, &results, N, M)
+        printSequence(&stack, &result, N, M)
         // 5. 스택에서 현재 index를 제거한다.
         stack.removeLast()
     }
@@ -35,11 +45,12 @@ if let input = readLine() {
     // [Array<Int>] stack (mutable): 현재 DFS로 탐색 중인 수를 저장하는 배열
     var stack = [Int]()
     
-    // [Array<String>] results (mutable): 생성된 수열을 저장하는 배열
-    var results = [String]()
+    // [String] results (mutable): 생성된 수열을 저장하는 문자열
+    var result: String = ""
     
     // DFS로 탐색하여 조건에 맞는 수열을 results에 저장한다.
-    printSequence(&stack, &results, N, M)
+    printSequence(&stack, &result, N, M)
     
-    print(results.joined(separator: "\n"))
+    // 결과 출력
+    print(result)
 }

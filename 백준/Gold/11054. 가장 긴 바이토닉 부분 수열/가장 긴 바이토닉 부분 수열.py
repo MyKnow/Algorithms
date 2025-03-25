8d410b2f131277b352606cc982126256be46e0c1
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-03-25 20:38:50 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 228 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,22 +1,35 @@
+# N: int
+# description: An integer that represents length of Ai
+# range: 1 ≤ N ≤ 1,000
 N = int(input())
 
+# Ai: list(int)
+# description: A list of integers that stores sequences for finding bitonic sequences
+# length: N
+# range: 1 ≤ Ai ≤ 1,000
 Ai = list(map(int, input().split()))
 
+# dp: list(int)
+# description: A list of integers to store lengths of LIS for each index
+# length: N
 dp = [1] * N
+
+# reverseDP: list(int)
+# description: A list of integers to store lengths of LDS for each index
+# length: N
 reverseDP = [1] * N
 
+# Update dp
 for i in range(N):
     for j in range(i):
-        if (Ai[i] > Ai[j]):
-            dp[i] = max(dp[i], dp[j]+1)
+        if Ai[i] > Ai[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
+# Update reverseDP
+for i in range(N-1, -1, -1):
+    for j in range(N-1, i, -1):
+        if Ai[i] > Ai[j]:
+            reverseDP[i] = max(reverseDP[i], reverseDP[j] + 1)
 
-Ai.reverse()
-for i in range(N):
-    for j in range(i):
-        if (Ai[i] > Ai[j]):
-            reverseDP[i] = max(reverseDP[i], reverseDP[j]+1)
-
-reverseDP.reverse()
-
+# Print by subtracting 1 from the maximum value from the sum of dp and reverseDP
 print(max(map(sum, zip(dp, reverseDP)))-1)

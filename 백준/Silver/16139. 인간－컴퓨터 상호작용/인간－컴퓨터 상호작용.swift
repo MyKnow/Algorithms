81b0812ca82aba10e16eae6efe6cf04baccb127a
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-04-02 18:57:13 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 444 ms, Memory: 126772 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.swift
@@ -1,69 +1,29 @@
-// S: String (immutable)
-// description: A string to solve problem
-// lenght: ≤ 200,000
-// restriction: only lowercase
-//
-// q: int (Not used)
-// description: A integer representing number of queries
-// range: 1 ≤ q ≤ 200,000
-if let S: String = readLine(), let qOfInput = readLine(), let _: Int = Int(qOfInput) {
-    // pSum: [[Int]] (mutable)
-    // description: 2-Dimensional array of integer to calculate a prefix sum of each alphabet count
-    // length: S.count * 26
-    var pSum = [[Int]](
-        repeating: [Int](
-            repeating: 0,
-            count: S.count
-        ),
-        count: 26
-    )
+import Foundation
 
-    for (i, c) in S.enumerated() {
-        // idx: UInt8 (immutable)
-        // description: Index of the alphabet "S[i]" in the range from 0 to 25
-        if let ascii: UInt8 = c.asciiValue {
-            let idx: Int = Int(ascii)-97
-            
-            // 1. Update the current index value of the alphabet S[i] 
-            // to the previous index value plus 1
-            pSum[idx][i] = pSum[idx][max(i-1, 0)] + 1
-            
-            // 2. Update other alphabet to the previous index value
-            for j in 0..<26 {
-                // Except current alphabet
-                if j != idx {
-                    pSum[j][i] = pSum[j][max(0, i-1)]
-                }
-            }
+if let S = readLine(), let _ = readLine() {
+    let n = S.count
+    let chars = Array(S.utf8) // ASCII 변환을 위해 Array로 변환
+    var pSum = Array(repeating: [Int](repeating: 0, count: n), count: 26)
+
+    pSum[Int(chars[0]) - 97][0] = 1 // 첫 번째 문자 초기화
+
+    for i in 1..<n {
+        let idx = Int(chars[i]) - 97
+        for j in 0..<26 {
+            pSum[j][i] = pSum[j][i-1] + (j == idx ? 1 : 0)
         }
     }
-    
-    // results: [String] (mutable)
-    // description: A array of strings to store each query's answer for quick output
-    // length: q
+
     var results = [String]()
 
-    // queries: String
-    // description: A array of String that store queries that include alphabet and range
-    // range: 0 ≤ l ≤ r < len(S)
-    // restriction: c is given in lowercase only.
-    while let query: String = readLine() {
-        let arrayOfQuery: [String] = query.split(separator: " ").map { String($0) }
-        
-        // Split queries to c, l, r
-        // c: current alphabet
-        // l, r: range for finding c
-        let c: Character = Character(arrayOfQuery[0])
-        if let ascii: UInt8 = c.asciiValue, 
-            let l: Int = Int(String(arrayOfQuery[1])), 
-            let r: Int = Int(String(arrayOfQuery[2])) {
-                
-            let cIdx: Int = Int(ascii)-97
-            // Obtain a number of c in range [l, r] and Append to results
-            results.append(String(pSum[cIdx][r] - (l-1 < 0 ? 0 : pSum[cIdx][l-1]) ))
-        }
+    while let query = readLine() {
+        let parts = query.split(separator: " ")
+        let cIdx = Int(parts[0].utf8.first!) - 97
+        let l = Int(parts[1])!
+        let r = Int(parts[2])!
+
+        results.append(String(pSum[cIdx][r] - (l > 0 ? pSum[cIdx][l-1] : 0)))
     }
-    
-    // Print answer
+
     print(results.joined(separator: "\n"))
 }

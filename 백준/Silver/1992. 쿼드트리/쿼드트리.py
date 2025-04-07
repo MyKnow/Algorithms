14cd2da6c00f530e9f54dd4cc0e06008246aa6ad
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-04-07 20:40:34 +0900

    [Silver I] Title: 쿼드트리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -3,54 +3,30 @@ import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 
-def isSingleColor(v: list(list())):
-    l = len(v)
-    
-    tmp = 0
+def isSingleColor(v: list[list[int]]) -> bool:
+    color = v[0][0]
     for row in v:
-        tmp += sum(row)
+        for val in row:
+            if val != color:
+                return False
+    return True
 
-    if tmp == 0 or tmp == l*l:
-        return True
-    else:
-        return False
-
-def divideVideo(v: list(list())):
-    l = len(v)
-    n = l // 2
-
-    tmp = [[[1 for _ in range(n)] for _ in range(n)] for _ in range(4)]
+def recursion(v: list(list())):
+    n = len(v)
     
-    for y in range(l):
-        for x in range(l):
-            color = v[y][x]
-            if y < n:
-                # 제 2사분면
-                if x < n:
-                    tmp[0][y][x] = color
-                # 제 1사분면
-                else:
-                    tmp[1][y][x-n] = color
-            else:
-                # 제 3사분면
-                if x < n:
-                    tmp[2][y-n][x] = color
-                # 제 4사분면
-                else:
-                    tmp[3][y-n][x-n] = color
-
-    return tmp
-
-def recursion(v: list(list()), n: int):
     if (n == 1 or isSingleColor(v) == True):
         return str(v[0][0])
 
-    subSquare = divideVideo(v)
-    
-    return f"({recursion(subSquare[0], n//2)}{recursion(subSquare[1], n//2)}{recursion(subSquare[2], n//2)}{recursion(subSquare[3], n//2)})"
+    half = n//2
+    return "(" + \
+        recursion([row[:half] for row in v[:half]]) + \
+        recursion([row[half:] for row in v[:half]]) + \
+        recursion([row[:half] for row in v[half:]]) + \
+        recursion([row[half:] for row in v[half:]]) + \
+        ")"
 
 N = int(input())
 
 video = [list(map(int, input().rstrip())) for _ in range(N)]
 
-print(f"{recursion(video, N)}\n")
+print(f"{recursion(video)}\n")

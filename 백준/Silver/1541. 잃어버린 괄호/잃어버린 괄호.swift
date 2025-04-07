f87ffdf6a58deab93a484876936ab0fb9f486324
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-04-07 18:19:45 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.swift
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.swift
@@ -3,10 +3,10 @@ if let inputOfExp: String = readLine() {
     // description: An array of strings that store each sub expression that splited by "-"
     let exp: [String] = inputOfExp.split(separator: "-").map { String($0) }
 
-    // splitByPlus: [[String]] (immutable)
-    // description: 2-Dimensional array of strings that store results of 
+    // splitByPlus: [Int] (immutable)
+    // description: An array of integers that store results of 
     // each sub expression that splited by "+" from exp
-    let splitByPlus = exp.map { $0.split(separator: "+").compactMap { Int($0) }.reduce(0, +) }
+    let splitByPlus: [Int] = exp.map { $0.split(separator: "+").compactMap { Int($0) }.reduce(0, +) }
     
     // Print result of subtracting between each sub expression
     print(splitByPlus[0] - (splitByPlus[1...].reduce(0, +)))

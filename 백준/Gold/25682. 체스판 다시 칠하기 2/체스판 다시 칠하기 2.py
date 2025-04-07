45d1390884771021475929ec1e92466eadf9c3c5
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-04-07 15:21:31 +0900

    [Gold IV] Title: 체스판 다시 칠하기 2, Time: 560 ms, Memory: 178036 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -25,24 +25,21 @@ for y in range(1, N+1):
     line = list(map(lambda x: x == "B", [c for c in input().rstrip()]))
 
     # crtB: Correct color of "black first board" in each index y
-    # crtW: Correct color of "white first board" in each index y
     crtB = (True if y % 2 == 1 else False)
-    crtW = not crtB
 
     for x in range(1, M+1):
         # If the current color is incorrect, add the sum of 1 and
         # 2D prefixes and update the current pSum.
         pSumOfB[y][x] = (0 if line[x-1] == crtB else 1) + pSumOfB[y-1][x] + pSumOfB[y][x-1] - pSumOfB[y-1][x-1]
-        pSumOfW[y][x] = (0 if line[x-1] == crtW else 1) + pSumOfW[y-1][x] + pSumOfW[y][x-1] - pSumOfW[y-1][x-1]
+        pSumOfW[y][x] = (0 if line[x-1] != crtB else 1) + pSumOfW[y-1][x] + pSumOfW[y][x-1] - pSumOfW[y-1][x-1]
 
-        # Change the color of each variable to the opposite color.
+        # Change the color of crtB to the opposite color.
         crtB = not crtB
-        crtW = not crtW
 
-# minOfB, minOfW: float
-# description: A floats to store minimum value of each pSum
-minOfB = float('inf')
-minOfW = float('inf')
+# minValue: float
+# description: A float to stores minimum value of each pSum
+# initial: float infinity
+minValue = float('inf')
 
 for yK in range(K, N+1):
     for xK in range(K, M+1):
@@ -50,9 +47,8 @@ for yK in range(K, N+1):
         sumOfB = pSumOfB[yK][xK] - pSumOfB[yK-K][xK] - pSumOfB[yK][xK-K] + pSumOfB[yK-K][xK-K]
         sumOfW = pSumOfW[yK][xK] - pSumOfW[yK-K][xK] - pSumOfW[yK][xK-K] + pSumOfW[yK-K][xK-K]
 
-        # Update minOfB and minOfW to smallest value
-        minOfB = min(minOfB, sumOfB)
-        minOfW = min(minOfW, sumOfW)
+        # Update minValue to smallest value
+        minValue = min(minValue, sumOfB, sumOfW)
 
-# Print minimum value between minOfW and minOfB
-print(str(min(minOfW, minOfB))+"\n")
+# Print answer
+print(str(minValue)+"\n") 

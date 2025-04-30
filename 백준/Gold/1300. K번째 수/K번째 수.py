5f9fd7267bf50bdc22d3c8636dbefc6e09872332
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-04-30 19:52:29 +0900

    [Gold I] Title: K번째 수, Time: 748 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,16 +1,23 @@
 N = int(input())
 K = int(input())
 
+# Index starts at 1
 start = 1
-end = N*N
+
+# Kth number isn't greater than K
+end = K
 
 while (start <= end):
     mid = (start+end) // 2
 
+    # Count of numbers less than "mid" in list B
     total = 0
+
     for i in range(1, N+1):
+        # mid // i : Column index smaller than "mid" value in row i
         total += min(mid // i, N)
 
+    # When "mid" smaller than B[K]
     if total < K:
         start = mid + 1
     else:

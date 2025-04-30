30c262d1c87a344dbc83784c11f9adaea736b63d
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-04-30 19:47:01 +0900

    [Gold I] Title: K번째 수, Time: 24 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.swift
@@ -0,0 +1,31 @@
+if let inputOfN: String = readLine(), let N: Int = Int(inputOfN),
+    let inputOfK: String = readLine(), let K: Int = Int(inputOfK) {
+        
+    // Index starts at 1
+    var start: Int = 1
+
+    // Last index of B
+    var end: Int = N*N
+
+    while (start <= end) {
+        let mid: Int = (start+end) / 2
+
+        // Count of numbers less than "mid" in list B
+        var total: Int = 0
+    
+        for i in 1...N {
+            // mid / i : Column index smaller than "mid" value in row i
+            total += min(mid / i, N)
+        }
+    
+        // When "mid" smaller than B[K]
+        if total < K {
+            start = mid + 1
+        }
+        else {
+            end = mid - 1
+        }
+    }
+        
+    print(end+1)   
+}

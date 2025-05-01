dac282b8dfcd6f55533928f46f86194c7b9d51ee
MyKnow <56116733+MyKnow@users.noreply.github.com>
2025-05-01 19:20:19 +0900

    [Silver II] Title: 최대 힙, Time: 84 ms, Memory: 47604 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,23 +1,22 @@
 import sys, heapq
 
-input = sys.stdin.read
+read = sys.stdin.read
 write = sys.stdout.write
+data = read().split()
 
-data = input().split()
-
-N = data[0]
 X = map(int, data[1:])
+
+push = heapq.heappush
+pop = heapq.heappop
 
 pq = []
 result = []
+
 for x in X:
     if x != 0:
-        # Make x negative to make max-heap, 
-        heapq.heappush(pq, -x)
+        # Make x negative to make max-heap
+        push(pq, -x)
     else:
-        if pq:
-            result.append( -heapq.heappop(pq) )
-        else:
-            result.append( 0 )
+        result.append(-pop(pq) if pq else 0)
 
-write("\n".join(map(str, result))+"\n")
+write("\n".join(map(str, result)) + "\n")

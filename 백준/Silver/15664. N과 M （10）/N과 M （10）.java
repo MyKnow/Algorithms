a24ed2a4a08543bdc708be31f6817bc9c4b7e8cd
MyKnow <myknow000@gmail.com>
2026-02-04 22:21:10 +0900

    [Silver II] Title: N과 M (10), Time: 68 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int N, M;
+  static int[] nums;
+  static int[] count;
+  static int[] arr;
+  static int size;
+  static StringBuilder sb = new StringBuilder();
+
+  static void dfs(int start, int depth) {
+    if (depth == M) {
+      for (int i = 0; i < M; i++) sb.append(arr[i]).append(' ');
+      sb.append('\n');
+      return;
+    }
+
+    for (int i = start; i < size; i++) {
+      if (count[i] == 0) continue;
+
+      count[i]--;
+      arr[depth] = nums[i];
+      dfs(i, depth + 1);
+      count[i]++;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    int[] input = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      input[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(input);
+
+
+    nums = new int[N];
+    count = new int[N];
+    size = 0;
+    for (int i = 0; i < N; i++) {
+      if (i == 0 || input[i] != input[i - 1]) {
+        nums[size] = input[i];
+        count[size] = 1;
+        size++;
+      } else {
+        count[size - 1]++;
+      }
+    }
+
+    arr = new int[M];
+    dfs(0, 0);
+    System.out.print(sb);
+  }
+}

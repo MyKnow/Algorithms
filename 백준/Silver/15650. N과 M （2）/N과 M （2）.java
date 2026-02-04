5c7b6ba91ee370ac523c19ba303ef7cb5b3f9c49
MyKnow <myknow000@gmail.com>
2026-02-04 16:56:39 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  static int N, M;
+  static boolean[] used;
+  static int[] arr;
+  static StringBuilder sb = new StringBuilder();
+
+  static void dfs(int start, int depth) {
+    if (depth == M) {
+      for (int i = 0; i < M; i++) sb.append(arr[i]).append(' ');
+      sb.append('\n');
+      return;
+    }
+
+    for (int i = start; i <= N; i++) {
+      if (used[i]) continue;
+      used[i] = true;
+      arr[depth] = i;
+      dfs(i+1, depth+1);
+      used[i] = false;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+
+    used = new boolean[N + 1];
+    arr = new int[M];
+
+    dfs(1, 0);
+    System.out.print(sb);
+  }
+}

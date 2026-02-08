2218c64cef56ee4218d5f851db7c8e4ba83237a7
MyKnow <myknow000@gmail.com>
2026-02-08 16:56:38 +0900

    [Gold V] Title: 치킨 배달, Time: 96 ms, Memory: 12396 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -18,110 +18,73 @@ import java.util.Arrays;
 // 집 최대 개수는 2N(100개, 최소 1개)
 // 치킨집의 개수는 M이상 13 이하
 
-
-
 public class Main {
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static StringBuilder sb = new StringBuilder();
   static int N, M;
-  static int[][] map;
   static int[][] chickenCoords = new int[13][2];
   static int[][] houseCoords = new int[100][2];
-  static int[][] chickenComb;
   static int[][] chickenDistances;
   static int chickenSize;
   static int houseSize;
-  static int combinationSize;
-  static int combinationIdx;
-  static boolean[] isSelected;
+  static int[] selected;
   static int answer;
-  
+
   static void init() throws Exception {
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    
-    map = new int[N][N];
+
     chickenSize = 0;
     houseSize = 0;
+
     for (int r=0; r<N; r++) {
       st = new StringTokenizer(br.readLine());
       for (int c=0; c<N; c++) {
         int current = Integer.parseInt(st.nextToken());
-        if (current == 0) continue;
-        else if (current == 1) houseCoords[houseSize++] = new int[] { r, c };
+        if (current == 1) houseCoords[houseSize++] = new int[] { r, c };
         else if (current == 2) chickenCoords[chickenSize++] = new int[] { r, c };
       }
     }
-    System.arraycopy(chickenCoords, 0, chickenCoords, 0, chickenSize);
-    System.arraycopy(houseCoords, 0, houseCoords, 0, houseSize);
-    combinationSize = calcCombination(chickenSize, M);
-    chickenComb = new int[combinationSize][M];
-    isSelected = new boolean[chickenSize];
-    combinationIdx = 0;
-    answer = Integer.MAX_VALUE;
-  }
-  
-  static int calcCombination(int n, int r) {
-    if (n==r || r == 0) return 1;
-    return calcCombination(n-1, r-1) + calcCombination(n-1, r);
-  }
-  
-  static void travelHouse() {
+
     chickenDistances = new int[houseSize][chickenSize];
     for (int i=0; i<houseSize; i++) {
-      int[] hCoords = houseCoords[i];
-      int houseR = hCoords[0];
-      int houseC = hCoords[1];
-      
       for (int j=0; j<chickenSize; j++) {
-        int[] cCoords = chickenCoords[j];
-        int chickenR = cCoords[0];
-        int chickenC = cCoords[1];
-        
-        chickenDistances[i][j] = Math.abs(chickenR-houseR) +Math.abs(chickenC-houseC);
+        chickenDistances[i][j] =
+          Math.abs(houseCoords[i][0] - chickenCoords[j][0]) +
+          Math.abs(houseCoords[i][1] - chickenCoords[j][1]);
       }
     }
+
+    selected = new int[M];
+    answer = Integer.MAX_VALUE;
   }
-  
-  static void updateCombination(int start, int depth) {
+
+  static void dfs(int start, int depth) {
     if (depth == M) {
-      int idx = 0;
-      for (int i=0; i<chickenSize; i++) {
-        if (isSelected[i]) chickenComb[combinationIdx][idx++] = i;
-      }
-      combinationIdx++;
-      return;
-    }
-    
-    for (int i=start; i<chickenSize; i++) {
-      isSelected[i] = true;
-      updateCombination(i+1, depth+1);
-      isSelected[i] = false;
-    }
-  }
-  
-  static void updateChickenDistance() {
-    for (int[] comb : chickenComb) {
       int sum = 0;
 
-      for (int hIdx = 0; hIdx < houseSize; hIdx++) {
+      for (int hIdx=0; hIdx<houseSize; hIdx++) {
         int minDist = Integer.MAX_VALUE;
-        for (int cIdx : comb) {
-          minDist = Math.min(minDist, chickenDistances[hIdx][cIdx]);
+        for (int i=0; i<M; i++) {
+          minDist = Math.min(minDist, chickenDistances[hIdx][selected[i]]);
         }
         sum += minDist;
+        if (sum >= answer) return;
       }
-      answer = Math.min(answer, sum);
-  }
-}
 
-  
+      answer = Math.min(answer, sum);
+      return;
+    }
+
+    for (int i=start; i<chickenSize; i++) {
+      selected[depth] = i;
+      dfs(i+1, depth+1);
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     init();
-    travelHouse();
-    updateCombination(0, 0);
-    updateChickenDistance();
+    dfs(0, 0);
     System.out.print(answer);
   }
 }

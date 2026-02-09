29edbf7a10331b9f607f26b042199ccefa6ca060
MyKnow <myknow000@gmail.com>
2026-02-09 23:27:52 +0900

    [Gold V] Title: 톱니바퀴, Time: 64 ms, Memory: 11748 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  
+  static int[][] magnet = new int[4][8];
+  static int K;
+  
+  static void init() throws Exception {
+    for (int i = 0; i < 4; i++) {
+      String line = br.readLine();
+      for (int j = 0; j < 8; j++) {
+        magnet[i][j] = line.charAt(j) - '0';
+      }
+    }
+    
+    K = Integer.parseInt(br.readLine());
+  }
+  
+  static void process() throws Exception {
+    for (int k = 0; k < K; k++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int idx = Integer.parseInt(st.nextToken()) - 1;
+      int dir = Integer.parseInt(st.nextToken());
+      simulate(idx, dir);
+    }
+  }
+  
+  static void simulate(int idx, int dir) {
+    int[] rotateDir = new int[4];
+    rotateDir[idx] = dir;
+    
+    // 왼쪽 전파
+    for (int i = idx - 1; i >= 0; i--) {
+      if (magnet[i][2] != magnet[i + 1][6]) {
+        rotateDir[i] = -rotateDir[i + 1];
+      } else {
+        break;
+      }
+    }
+    
+    // 오른쪽 전파
+    for (int i = idx + 1; i < 4; i++) {
+      if (magnet[i - 1][2] != magnet[i][6]) {
+        rotateDir[i] = -rotateDir[i - 1];
+      } else {
+        break;
+      }
+    }
+    
+    // 회전 실행
+    for (int i = 0; i < 4; i++) {
+      if (rotateDir[i] == 1) rotateClockwise(i);
+      else if (rotateDir[i] == -1) rotateCounterClockwise(i);
+    }
+  }
+  
+  static void rotateClockwise(int idx) {
+    int temp = magnet[idx][7];
+    for (int i = 7; i > 0; i--) {
+      magnet[idx][i] = magnet[idx][i - 1];
+    }
+    magnet[idx][0] = temp;
+  }
+  
+  static void rotateCounterClockwise(int idx) {
+    int temp = magnet[idx][0];
+    for (int i = 0; i < 7; i++) {
+      magnet[idx][i] = magnet[idx][i + 1];
+    }
+    magnet[idx][7] = temp;
+  }
+  
+  static int calcScore() {
+    int score = 0;
+    for (int i = 0; i < 4; i++) {
+      if (magnet[i][0] == 1) {
+        score += (1 << i);
+      }
+    }
+    return score;
+  }
+  
+  public static void main(String[] args) throws Exception {
+    init();
+    process();
+    System.out.println(calcScore());
+  }
+}

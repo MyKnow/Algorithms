6e3f3dc36125e175574fc99062b9784467756095
MyKnow <myknow000@gmail.com>
2026-02-16 18:07:21 +0900

    [Gold IV] Title: 알파벳, Time: 444 ms, Memory: 52500 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class Main {
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   static StringBuilder sb = new StringBuilder();
-  static int answer;
+  static int maxDepth;
   
   static int R, C;
-  static int[][] map;
+  static int[][] board;
+  static HashSet<Integer>[][] visitedMask;
   
   static int dr[] = { -1, 1, 0, 0 };
   static int dc[] = {  0, 0,-1, 1 };
@@ -18,22 +20,26 @@ public class Main {
     R = Integer.parseInt(st.nextToken());
     C = Integer.parseInt(st.nextToken());
     
-    map = new int[R][C];
+    board = new int[R][C];
+    visitedMask = (HashSet<Integer>[][]) new HashSet[R][C];
     for (int r=0; r<R; r++) {
       String line = br.readLine();
       for (int c=0; c<C; c++) {
-        map[r][c] = line.charAt(c)-'A';
+        board[r][c] = line.charAt(c)-'A';
+        visitedMask[r][c] = new HashSet<Integer>();
       }
     }
   }
   
   static void solve() {
-    int initMask = masking(0, map[0][0]);
-    dfs(0, 0, initMask, 1);
+    int mask = setBit(0, board[0][0]);
+    visitedMask[0][0].add(mask);
+    dfs(0, 0, mask, 1);
   }
   
   static void dfs(int r, int c, int mask, int depth) {
-    answer = Math.max(answer, depth);
+    maxDepth = Math.max(maxDepth, depth);
+    if (maxDepth == 'Z'-'A'+1) return;
     
     for (int d=0; d<dr.length; d++) {
       int nr = r+dr[d];
@@ -41,10 +47,13 @@ public class Main {
       
       if (!isInRange(nr, nc)) continue;
       
-      int nv = map[nr][nc];
+      int nv = board[nr][nc];
       if (isAlreadyMasked(mask, nv)) continue;
       
-      int nm = masking(mask, nv);
+      int nm = setBit(mask, nv);
+      if (isAlreadyVisited(nr, nc, nm)) continue;
+      
+      visitedMask[nr][nc].add(nm);
       dfs(nr, nc, nm, depth+1);
     }
   }
@@ -57,14 +66,18 @@ public class Main {
     return (mask & (1 << value)) != 0;
   }
   
-  static int masking(int mask, int value) {
+  static int setBit(int mask, int value) {
     return mask | (1 << value);
+  }
+  
+  static boolean isAlreadyVisited(int nr, int nc, int mask) {
+    return visitedMask[nr][nc].contains(mask);
   }
   
   public static void main(String[] args) throws Exception {
     init();
     solve();
-    sb.append(answer).append("\n");
+    sb.append(maxDepth).append("\n");
     System.out.print(sb.toString());
   }
 }

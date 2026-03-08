fc5b4f06fe1456bbdb8687730a87b9c3b1014c30
MyKnow <myknow000@gmail.com>
2026-03-08 17:20:26 +0900

    [Gold III] Title: 일감호에 다리 놓기, Time: 1464 ms, Memory: 338468 KB -BaekjoonHub

---
+++ 백준/Gold/17490. 일감호에 다리 놓기/일감호에 다리 놓기.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder sb = new StringBuilder();
+
+  static int N, M;
+  static long K;
+
+  static int[] parent;
+  static List<Edge> edges;
+
+  static class Edge {
+    int u, v;
+    long w;
+
+    Edge(int u, int v, long w) {
+      this.u = u;
+      this.v = v;
+      this.w = w;
+    }
+  }
+
+  static void init() throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    K = Long.parseLong(st.nextToken());
+
+    parent = new int[N+1];
+
+    for (int i=1;i<=N;i++) parent[i] = i-1;
+    parent[1] = N;
+
+    st = new StringTokenizer(br.readLine());
+    edges = new ArrayList<>();
+    for (int i=1;i<=N;i++) {
+      long w = Long.parseLong(st.nextToken());
+      edges.add(new Edge(0,i,w));
+    }
+
+    for (int i=0;i<M;i++) {
+      st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+
+      int min = Math.min(a,b);
+      int max = Math.max(a,b);
+
+      if (min==1 && max==N) {
+        parent[1] = 1;
+        continue;
+      }
+
+      parent[max] = max;
+    }
+  }
+
+  static int find(int x) {
+    if (parent[x]==x) return x;
+    return parent[x] = find(parent[x]);
+  }
+
+  static boolean union(int a,int b) {
+    int pa = find(a);
+    int pb = find(b);
+
+    if (pa==pb) return false;
+
+    parent[pa] = pb;
+    return true;
+  }
+
+  static void solve() {
+
+    if (M<=1) {
+      sb.append("YES");
+      return;
+    }
+
+    Collections.sort(edges,(a,b)->Long.compare(a.w,b.w));
+
+    long sum = 0;
+
+    for (Edge e: edges) {
+      if (union(e.u,e.v)) sum += e.w;
+    }
+
+    sb.append(sum>K?"NO":"YES");
+  }
+
+  public static void main(String[] args) throws Exception {
+    init();
+    solve();
+    System.out.println(sb);
+  }
+}

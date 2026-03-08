0c18f7c1061f2eeef17531fa59fc5bdba3b36b5f
MyKnow <myknow000@gmail.com>
2026-03-08 18:06:28 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1396 ms, Memory: 324360 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringBuilder sb = new StringBuilder();
+  
+  static long sum;
+  
+  static int N;
+  static int M;
+  static int[] parent;
+  static List<Edge> edge;
+  
+  static class Edge implements Comparable<Edge> {
+    int u, v, w;
+    
+    Edge(int u, int v, int w) {
+      this.u = u;
+      this.v = v;
+      this.w = w;
+    }
+    
+    public int compareTo(Edge e) {
+      return Integer.compare(this.w, e.w);
+    }
+  }
+  
+  static void init() throws Exception {
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    
+    parent = new int[N+1];
+    for (int i=1; i<=N; i++) parent[i] = i;
+    
+    edge = new ArrayList<>();
+    for (int i=0; i<M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+      int weight = Integer.parseInt(st.nextToken());
+      
+      edge.add( new Edge(from, to, weight) );
+    }
+  }
+  
+  static void solve() {
+    int count = 0;
+    int maxEdge = 0;
+  
+    Collections.sort(edge);
+  
+    for (Edge e: edge) {
+      if (union(e.u, e.v)) {
+        sum += e.w;
+        maxEdge = e.w;
+        count++;
+  
+        if (count == N-1) break;
+      }
+    }
+  
+    sum -= maxEdge;
+  }
+  
+  static int find(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = find(parent[x]);
+  }
+  
+  static boolean union(int a, int b) {
+    a = find(a);
+    b = find(b);
+    
+    if (a == b) return false;
+    parent[b] = a;
+    return true;
+  }
+  
+  public static void main(String[] args) throws Exception {
+    init();
+    solve();
+    sb.append(sum).append("\n");
+    System.out.print(sb.toString());
+  }
+}
